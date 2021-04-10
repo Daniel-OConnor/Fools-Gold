bc1a35a9dee377e4cce00031b22bf5cd3ef8b3fd
faiz <faiz95ahmed@gmail.com>
2021-04-10 15:30:08 +0100

    made lv data gen more robust

--- lv_generator.py
+++ lv_generator.py
@@ -8,7 +8,8 @@ from torch.utils.data import DataLoader
 
 num_priors_total = 16
 num_workers = 4
-priors_per_worker = num_priors_total // num_workers
+num_iterations = 2
+num_priors_per_iteration = num_priors_total // num_iterations
 
 prior = lambda: lotkavolterra.generate_prior(torch.rand(4), width=0.02)
 sim = LotkaVolterra(normalisation_func=normalisation_func_brehmer)
@@ -24,7 +25,8 @@ def foo(i):
     score = sim.eval_score(zs, θ).detach()
     return (0, (θ, default_params), zs[-1], score, ratio)
 
-with Pool(num_workers) as p:
-    dataset = list(tqdm(p.imap(foo, range(num_priors_total)), total=num_priors_total))
-
-torch.save(dataset, 'lv_data.pt')
+for i in range(num_iterations):
+    print("Iteration {} of {}...".format(i+1, num_iterations))
+    with Pool(num_workers) as p:
+        dataset = list(tqdm(p.imap(foo, range(num_priors_per_iteration)), total=num_priors_per_iteration))
+    torch.save(dataset, 'lv_data_{}.pt'.format(i))
