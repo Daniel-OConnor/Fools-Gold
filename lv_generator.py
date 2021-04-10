6e47093a1285714f09782bcd7ff96dca04fed21a
faiz <faiz95ahmed@gmail.com>
2021-04-10 21:36:16 +0100

    updated lv_generator

--- lv_generator.py
+++ lv_generator.py
@@ -6,13 +6,13 @@ from tqdm import tqdm
 import torch
 from torch.utils.data import DataLoader
 
-num_priors_total = 16
-num_workers = 4
-num_iterations = 2
+num_priors_total = 16 # EDIT
+num_workers = 4 # EDIT
+num_iterations = 2 # EDIT
 num_priors_per_iteration = num_priors_total // num_iterations
 
-prior = lambda: lotkavolterra.generate_prior(torch.rand(4), width=0.02)
-sim = LotkaVolterra(normalisation_func=normalisation_func_brehmer)
+prior = lambda: lotkavolterra.generate_prior(torch.rand(4), width=0.02) # EDIT
+sim = LotkaVolterra(normalisation_func=normalisation_func_brehmer) # EDIT
 
 print("Generating data...")
 
@@ -20,13 +20,29 @@ def foo(i):
     θ = prior().detach().requires_grad_()
     with torch.no_grad():
         zs = sim.simulate(θ)
-    with torch.no_grad():
-        ratio = sim.eval_ratio(zs, θ, default_params).detach()
-    score = sim.eval_score(zs, θ).detach()
-    return (0, (θ, default_params), zs[-1], score, ratio)
+        logp_0 = sim.eval_ratio(zs, θ, ).detach()
+        logp_1 = sim.eval_ratio(zs, default_params).detach()
+    score0 = sim.eval_score(zs, θ).detach()
+    score1 = sim.eval_score(zs, default_params).detach()
+    return (0, (θ, default_params), zs[-1], (score0, score1), (logp_0, logp_1))
 
 for i in range(num_iterations):
     print("Iteration {} of {}...".format(i+1, num_iterations))
     with Pool(num_workers) as p:
         dataset = list(tqdm(p.imap(foo, range(num_priors_per_iteration)), total=num_priors_per_iteration))
     torch.save(dataset, 'lv_data_{}.pt'.format(i))
+
+# EXAMPLE LOADING CODE
+
+# dataset = []
+# for i in range(num_files):
+#     saved_data = torch.load("../lv_data/lv_data_{}.pt".format(i))
+#     for (_, (t1, t2), x, (score0, score1), (logp_0, logp_1)) in saved_data:
+#         t1.requires_grad_()
+#         t2.requires_grad_()
+#         x.requires_grad_()
+#         score0.requires_grad_()
+#         score1.requires_grad_()
+#         logp_0.requires_grad_()
+#         logp_1.requires_grad_()
+#     dataset = dataset + saved_data
