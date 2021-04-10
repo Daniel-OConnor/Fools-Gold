af316a448bbf361d5a5f1c3cd2d7ebe7e864d0e9
Daniel O'Connor <danieloconnor@keble.ox.ac.uk>
2021-04-10 16:50:19 +0100

    Plotting and training code for LV sim

--- main.py
+++ main.py
@@ -21,25 +21,24 @@ import matplotlib.pyplot as plt
 
 TRAIN = True
 # training constants
-batch_size = 2 #32
-epochs = 5
+batch_size = 20 #32
+epochs = 300
 train_fraction = 1
-num_priors = 4000 #30000
+num_priors = 100 #30000
 num_sims_per_prior_pair = 1
 learning_rate = 0.00001
 num_train_priors = int(num_priors * train_fraction)
 num_test_priors = int(num_priors * (1-train_fraction))
 
-prior = lambda: ((torch.rand(1).to(device))*0.25)
-sim = GaltonBoard(10, 10)
+prior = lambda: lotkavolterra.generate_prior(torch.rand(4), width=0.25).to(device)
+sim = LotkaVolterra(normalisation_func=normalisation_func_brehmer)
 
 device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
 if torch.cuda.is_available():
     torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
-model = CategoricalDistribution(sim.bins, sim.theta_size, 100)
+model = DensityMixture(sim.x_size, sim.theta_size, 10, 1000)
 model.to(device)
-
 
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 if TRAIN:
@@ -49,7 +48,7 @@ if TRAIN:
     #test_loader = ratio_dataset(sim, prior, num_test_priors, num_sims_per_prior_pair, batch_size, False)
 
     # %% TRAIN
-    train(model, train_loader, partial(scandal, alpha=3, prob_func=categorical_prob), epochs, optimizer)
+    train(model, train_loader, partial(scandal, alpha=3, prob_func=gaussian_mixture_prob), epochs, optimizer)
 
     torch.save(model.state_dict(), "model.pt")
 else:
@@ -64,31 +63,34 @@ else:
 # density_pred.covariance_factor = lambda : .01
 # density_pred._compute_covariance()
 
-"""
-theta0 = 0.2
+theta0 = torch.Tensor([0.01, 0.5, 1, 0.01])
 theta1 = 0.8
-# generate data for a single pair of thetas
-visual_runs0 = np.array([sim.simulate(theta0)[1].cpu().detach().numpy() for _ in tqdm(range(10000))])
-visual_runs1 = np.array([sim.simulate(theta1)[1].cpu().detach().numpy() for _ in tqdm(range(10000))])
 
-xs = np.linspace(-5, 5, 500)
-density_true0 = gaussian_kde(visual_runs0)
-density_true0.covariance_factor = lambda : .1
-density_true0._compute_covariance()
-density_true1 = gaussian_kde(visual_runs1)
-density_true1.covariance_factor = lambda : .1
-density_true1._compute_covariance()
+x = np.linspace(0, 1, 100)
+xs = np.reshape(x, (x.shape[0], 1))
+xs = np.concatenate((xs, np.array([[-0.1, 4000, 2000, 0.2, 0.2, 0.02, 0.2, -0.2]]*xs.shape[0])), axis=1)
 
-
-_, mean, sd, weight = model(torch.tensor([[0]], dtype=torch.float32), torch.tensor([[theta0]], dtype=torch.float32))
-#density_pred = [(1-x)/x for x in density_pred]
+_, mean, sd, weight = model(torch.tensor([[0]], dtype=torch.float32), torch.unsqueeze(theta0, 0))
 with torch.no_grad():
-    density_pred = [torch.exp(gaussian_mixture_prob(torch.tensor(x, dtype=torch.float32), mean, sd, weight)) for x in xs]
+    density_pred = [torch.exp(gaussian_mixture_prob(torch.tensor([x], dtype=torch.float32), mean, sd, weight)) for x in xs]
+print(mean, xs[0])
+#print(mean, sd)
+# generate data for a single pair of thetas
+with torch.no_grad():
+    visual_runs0 = np.array([sim.simulate(theta0)[-1].cpu().detach().numpy() for _ in tqdm(range(100))])
+#visual_runs1 = np.array([sim.simulate(theta1)[1].cpu().detach().numpy() for _ in tqdm(range(1000))])
+density_true0 = gaussian_kde(np.swapaxes(visual_runs0, 0, 1))
+"""density_true1 = gaussian_kde(visual_runs1)
+density_true1.covariance_factor = lambda : .1
+density_true1._compute_covariance()"""
 
-plt.plot(xs, density_true0(xs), "r")
+
+#density_pred = [(1-x)/x for x in density_pred]
+
+plt.plot(x, density_true0(np.swapaxes(xs, 0, 1)), "r")
 #plt.plot(xs, density_true0(xs), "y")
 #plt.plot(xs, density_true1(xs), "g")
-plt.plot(xs, density_pred, "b")
+plt.plot(x, density_pred, "b")
 plt.show()
-"""
+
 print("Done")
