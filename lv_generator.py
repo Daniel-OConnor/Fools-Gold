00dd56593ca97295223a675700e7b93d00156d0b
faiz <faiz95ahmed@gmail.com>
2021-04-11 21:35:13 +0100

    updated lv_generator.py to allow for test data

--- lv_generator.py
+++ lv_generator.py
@@ -7,22 +7,23 @@ import torch
 from torch.utils.data import DataLoader
 
 start_num = 0
-num_priors_total = 16 # EDIT
+num_samples_total = 16 # EDIT
 num_workers = 4 # EDIT
 num_iterations = 2 # EDIT
-prefix = "lv_data_" # EDIT
+prefix = "lv_data_" # EDIT "lv_test_data_"
 extension = "pt" # EDIT
-save_loc = "lv_data" # EDIT
-prior = lambda: lotkavolterra.generate_prior(torch.rand(4), width=0.02) # EDIT
+save_loc = "lv_data" # EDIT "lv_test_data"
+prior0 = lambda: lotkavolterra.generate_prior(torch.rand(4), width=0.02).detach() # EDIT: e.g change this to some fixed value for generating test data
+prior1 = lambda: default_params.detach() # EDIT
 sim = LotkaVolterra(normalisation_func=normalisation_func_brehmer) # EDIT
 
-num_priors_per_iteration = num_priors_total // num_iterations
+num_samples_per_iteration = num_samples_total // num_iterations
 print("Generating data...")
 
-def foo(label):
+def foo(label, prior0, prior1):
     try:
-        θ_0 = prior().detach().requires_grad_()
-        θ_1 = default_params.detach().requires_grad_() # prior().detach().requires_grad_()
+        θ_0 = prior0.requires_grad_()
+        θ_1 = prior1.requires_grad_()
         with torch.no_grad():
             if label == 1:
                 zs = sim.simulate(θ_1)
@@ -41,20 +42,20 @@ def foo(label):
     except:
         return None
 
-save_iter = tqdm(range(start_num, num_iterations))
+save_iter = tqdm(range(start_num, num_iterations + start_num))
 total_runs = 0
 saved_samples = 0
 
 for i in save_iter:
     with Pool(num_workers) as p:
-        random_nums = torch.rand(num_priors_per_iteration)
-        labels = [1 if random_nums[i] > 0.5 else 0 for i in range(num_priors_per_iteration)]
+        random_nums = torch.rand(num_samples_per_iteration)
+        labels = [(1, prior0(), prior1()) if random_nums[i] > 0.5 else 0 for i in range(num_samples_per_iteration)]
         num_defaults += sum(labels)
         res = list(p.imap(foo, labels))
         total_runs += len(res)
         dataset = [t for t in res if t is not None]
         saved_samples += len(dataset)
-        save_iter.set_description("Yield: {}, Default Fraction: {}".format(saved_samples/total_runs, num_defaults/total_runs))
+        save_iter.set_description("Yield: {}, count(prior1)/total: {}".format(saved_samples/total_runs, num_defaults/total_runs))
     torch.save(dataset, "{}/{}{}.{}".format(save_loc, prefix, i, extension))
     del res, dataset
 
