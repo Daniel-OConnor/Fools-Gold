9ae85fb2cd3390fe2bde635a24444b5ce24e1bf2
faiz <faiz95ahmed@gmail.com>
2021-04-13 06:09:06 +0100

    fixed?

--- experiments/lv_trainer.py
+++ experiments/lv_trainer.py
@@ -11,6 +11,12 @@ from tqdm import tqdm
 import torch
 from functools import partial
 from pathlib import Path
+from sys import argv
+
+if len(argv) > 1:
+    start_from = argv[1]
+else:
+    start_from = 0
 
 # training constants
 batch_size = 32 #32
@@ -26,33 +32,37 @@ if torch.cuda.is_available():
 
 training_info = {"base": {"x_size": 9, "theta_size": 4, "optimizer": torch.optim.Adam, "learning_rate": 0.001}}
 
-training_info["rolr"].update("model" = lambda: Ratio(9, 4, 50), "hidden_size": 50, "loader": load_ratio_dataset, "loss"= rolr)
+training_info["rolr"].update({"model" : lambda: Ratio(9, 4, 50), "hidden_size": 50, "loader": load_ratio_dataset, "loss": rolr})
 
-training_info["rascal"] = training_info["rolr"].copy()
-training_info["rascal"].update("loader" = load_score_and_ratio_dataset, "alpha" = 0.1, "loss"= partial(rascal, alpha=0.1))
+training_info["rascal"] : training_info["rolr"].copy()
+training_info["rascal"].update({"loader" : load_score_and_ratio_dataset, "alpha" : 0.1, "loss": partial(rascal, alpha=0.1)})
 
-training_info["cascal"] = training_info["rascal"].copy()
-training_info["cascal"].update("model"= lambda: Classifier(9, 4, 50), "loader" = load_score_pairs_dataset, "alpha" = 0.1, "loss"=partial(cascal, alpha=0.1))
+training_info["cascal"] : training_info["rascal"].copy()
+training_info["cascal"].update({"model": lambda: Classifier(9, 4, 50), "loader" : load_score_pairs_dataset, "alpha" : 0.1, "loss":partial(cascal, alpha=0.1)})
 
-training_info["LRT"] = training_info["cascal"].copy()
-training_info["LRT"].update("loss"=xe)
+training_info["LRT"] : training_info["cascal"].copy()
+training_info["LRT"].update({"loss":xe})
 
-training_info["NDE"] = training_info["base"].copy()
-training_info["NDE"].update("model" = lambda: DensityMixture(9, 4, 10, 50), "loader" = load_score_dataset, "loss"=partial(cross_entropy, gaussian_mixture_prob))
+training_info["NDE"] : training_info["base"].copy()
+training_info["NDE"].update({"model" : lambda: DensityMixture(9, 4, 10, 50), "loader" : load_score_dataset, "loss":partial(cross_entropy, gaussian_mixture_prob)})
 
-training_info["scandal"] = training_info["NDE"].copy()
-training_info["scandal"].update("loss": partial(scandal, alpha=0.1, prob_func=gaussian_mixture_prob), "alpha" = 0.1)
+training_info["scandal"] : training_info["NDE"].copy()
+training_info["scandal"].update({"loss": partial(scandal, alpha=0.1, prob_func=gaussian_mixture_prob), "alpha" : 0.1})
 
-training_info["scandal2"] = training_info["scandal"].copy()
-training_info["scandal2"].update("loss": partial(scandal, alpha=0.01, prob_func=gaussian_mixture_prob), "alpha" = 0.01)
+training_info["scandal2"] : training_info["scandal"].copy()
+training_info["scandal2"].update({"loss": partial(scandal, alpha=0.01, prob_func=gaussian_mixture_prob), "alpha" : 0.01})
+
 
 dataset_sizes = [1000, 2000, 5000, 10000, 20000, 50000, 100000]
 pbar = tqdm(total = 490)
+num_completed = 0
 for experiments in experiments:
     Path("models/{}".format(experiment)).mkdir(parents=True, exist_ok=True)
     for data_size in dataset_sizes:
         for model_id in range(10):
             pbar.update(1)
+            if num_completed < start_from:
+                continue
             name = "{}_{}_{}".format(experiment, str(data_size), str(model_id))
             model = training_info[experiment]["model"]()
             model.to(device)
@@ -61,5 +71,6 @@ for experiments in experiments:
             for i in range(epochs):
                 train(model, train_loader, training_info[experiment]["loss"], i, optimizer)
             torch.save(model.state_dict(), "models/{}/{}.pt".format(experiment, name))
+            num_completed +=1
 
 
